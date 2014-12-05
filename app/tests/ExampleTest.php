192cf548b7d71bc1e49a261062f1051fcdf5e4fd
Thibault Derousseaux <tib.dex@gmail.com>
2014-12-05 15:11:56 +0100

    Ugly var_dumps to check environment on Shippable

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -9,6 +9,10 @@ class ExampleTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
+        var_dump($_SERVER);
+        var_dump($_ENV);
+        var_dump(gethostname());
+
         $this->client->request('GET', '/');
 
 		$this->assertTrue($this->client->getResponse()->isOk());
