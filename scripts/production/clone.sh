67ad122cf2f51f7e9a0a931b6bd11c76cd7181bb
Thibault Derousseaux <tib.dex@gmail.com>
2014-12-09 15:59:33 +0100

    Test weird stuff [skip ci] 5

--- scripts/production/clone.sh
+++ scripts/production/clone.sh
@@ -18,7 +18,8 @@ echo "Adding GitHub to the known hosts"
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 echo "Cloning the repository"
-git clone git@github.com:GroupEat/groupeat-web.git
+#git clone git@github.com:GroupEat/groupeat-web.git
+cd ~vagrant/groupeat
 git pull
 
 #if [ -d ~vagrant/groupeat ]; then
