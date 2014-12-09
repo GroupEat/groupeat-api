739aa71ef26e3a4124ab13e651558edcfe5366ec
Thibault Derousseaux <tib.dex@gmail.com>
2014-12-09 18:56:51 +0100

    Debug deployment 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+cat /home/shippable/.ssh/id_rsa
+cat /home/shippable/.ssh/id_rsa.pub
 ssh -v vagrant@178.62.158.190 "bash -s" < $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/production/deploying.sh
