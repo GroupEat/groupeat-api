a864b20270b73a33e94c898504e00cd2dd3d7728
Thibault Derousseaux <tib.dex@gmail.com>
2015-02-19 17:34:01 +0100

    Adding a log route for easy debugging

--- workbench/groupeat/support/src/routes.php
+++ workbench/groupeat/support/src/routes.php
@@ -9,4 +9,20 @@ foreach([403 => 'Forbidden', 404 => 'Not found', 500 => 'Internal error', 503 =>
             'title' => "$code: $title",
         ], $code);
     }]);
+
+    // TODO: Remove this block before app launch
+    Route::api(['version' => 'v1', 'protected' => false], function()
+    {
+        Route::get('log', function()
+        {
+            $data['data'] = [
+                'url' => \Request::fullUrl(),
+                'headers' => \Request::header(),
+            ];
+
+            \Log::debug($data['data']);
+
+            return $data;
+        });
+    });
 }
