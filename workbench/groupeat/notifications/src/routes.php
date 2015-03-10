650baf01817ce3de95703c3861117a556145b041
Thibault Derousseaux <tib.dex@gmail.com>
2015-03-10 17:24:27 +0100

    Trying to fix notifications route 2

--- workbench/groupeat/notifications/src/routes.php
+++ workbench/groupeat/notifications/src/routes.php
@@ -7,20 +7,22 @@ use Sly\NotificationPusher\Model\Message;
 use Sly\NotificationPusher\Model\Push;
 use Sly\NotificationPusher\PushManager;
 
-Route::get('api/send-push-notification/{deviceId}', function ($deviceId) {
-    $pushManager = new PushManager(PushManager::ENVIRONMENT_DEV);
-
-    $gcmAdapter = new \Sly\NotificationPusher\Adapter\Gcm([
-        'apiKey' => 'AIzaSyBjyfcgeWD4UlHxANBs5-6rupcGp0_u1V0',
-    ]);
-
-    $devices = new DeviceCollection([new Device($deviceId)]);
-
-    $message = new Message('This is an example.');
-
-    $push = new Push($gcmAdapter, $devices, $message);
-    $pushManager->add($push);
-    $pushManager->push();
-
-    echo 'Sent to device ID: '.$deviceId;
+Route::api(['version' => 'v1'], function () {
+    Route::get('send-push-notification/{deviceId}', function ($deviceId) {
+        $pushManager = new PushManager(PushManager::ENVIRONMENT_DEV);
+    
+        $gcmAdapter = new \Sly\NotificationPusher\Adapter\Gcm([
+            'apiKey' => 'AIzaSyBjyfcgeWD4UlHxANBs5-6rupcGp0_u1V0',
+        ]);
+    
+        $devices = new DeviceCollection([new Device($deviceId)]);
+    
+        $message = new Message('This is an example.');
+    
+        $push = new Push($gcmAdapter, $devices, $message);
+        $pushManager->add($push);
+        $pushManager->push();
+    
+        echo 'Sent to device ID: '.$deviceId;
+    });
 });
