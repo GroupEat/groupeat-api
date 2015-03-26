34cf52439a23e7345b5fe92f2de7ce0f52bac8a6
Thibault Derousseaux <tib.dex@gmail.com>
2015-03-26 09:55:26 +0100

    Generating the docs during deployment

--- .rocketeer/events.php
+++ .rocketeer/events.php
@@ -5,7 +5,7 @@ use Rocketeer\Facades\Rocketeer;
 Rocketeer::listenTo('deploy.before-symlink', function ($task) {
     $path = $task->releasesManager->getCurrentReleasePath();
 
-    foreach (['optimize', 'route:cache', 'config:cache', 'opcache:clear'] as $command) {
+    foreach (['optimize', 'route:cache', 'config:cache', 'opcache:clear', 'api:docs'] as $command) {
         $task->run("cd $path; php artisan $command");
     }
 });
