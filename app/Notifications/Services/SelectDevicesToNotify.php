1469a31c83506c41b3d55f936b681cc33e467878
Thibault Derousseaux <tib.dex@gmail.com>
2015-07-06 23:57:16 +0200

    Adding the noNotificationAfter feature to the selectDevicesToNotify class

--- app/Notifications/Services/SelectDevicesToNotify.php
+++ app/Notifications/Services/SelectDevicesToNotify.php
@@ -3,14 +3,17 @@ namespace Groupeat\Notifications\Services;
 
 use Carbon\Carbon;
 use Closure;
+use DB;
 use Groupeat\Customers\Entities\Customer;
 use Groupeat\Devices\Entities\Device;
 use Groupeat\Devices\Entities\Status;
 use Groupeat\Orders\Entities\DeliveryAddress;
 use Groupeat\Orders\Entities\GroupOrder;
 use Groupeat\Orders\Entities\Order;
+use Groupeat\Orders\Migrations\OrdersMigration;
 use Groupeat\Orders\Values\JoinableDistanceInKms;
 use Groupeat\Settings\Entities\CustomerSettings;
+use Groupeat\Settings\Migrations\CustomerSettingsMigration;
 use Illuminate\Support\Collection;
 
 class SelectDevicesToNotify
@@ -64,11 +67,23 @@ class SelectDevicesToNotify
 
     private function getCustomersThatCanBeNotifiedIds(GroupOrder $groupOrder, Collection $potentialCustomersToNotifyIds)
     {
-        $query = CustomerSettings::query()
-            ->where(CustomerSettings::NOTIFICATIONS_ENABLED, true)
-            ->where(CustomerSettings::NO_NOTIFICATION_AFTER, '>', Carbon::now()->toTimeString())
-            ->whereIn('customerId', $potentialCustomersToNotifyIds->all());
+        $ordersTable = OrdersMigration::TABLE;
+        $customerSettingsTable = CustomerSettingsMigration::TABLE;
+        $orderEntity = new Order;
+        $customerSettingEntity = new CustomerSettings;
 
-        return $query->lists('customerId');
+        $sql = 'SELECT DISTINCT ON ('.$orderEntity->getRawTableField('customerId').') '.$orderEntity->getRawTableField('customerId')
+            .' FROM '.$ordersTable
+            .' LEFT JOIN '.$customerSettingsTable
+            .' ON '.$orderEntity->getRawTableField('customerId').' = '.$customerSettingEntity->getRawTableField('customerId')
+            .' WHERE '.$orderEntity->getRawTableField('customerId').' IN ('.implode(',', $potentialCustomersToNotifyIds->all()).')'
+            .' AND '.$customerSettingEntity->getRawTableField(CustomerSettings::NOTIFICATIONS_ENABLED).' = true'
+            .' AND '.$customerSettingEntity->getRawTableField(CustomerSettings::NO_NOTIFICATION_AFTER)
+                ." > '".Carbon::now()->toTimeString()."'"
+            .' AND '.$customerSettingEntity->getRawTableField(CustomerSettings::DAYS_WITHOUT_NOTIFYING)
+                .' >= DATE_PART(\'day\', NOW()::timestamp - '.$orderEntity->getRawTableField('createdAt').'::timestamp)'
+            .' ORDER BY '.$orderEntity->getRawTableField('customerId').', '.$orderEntity->getRawTableField('createdAt').' DESC';
+
+        return collect(collect(DB::select(DB::raw($sql)))->lists('customerId'));
     }
 }
