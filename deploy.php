9460afe2a90a6994ef96ca2294ccc9a594adaf53
Thibault Derousseaux <tib.dex@gmail.com>
2015-07-22 22:36:57 +0200

    Rocketeer should be installed globally to deploy

--- deploy.php
+++ deploy.php
@@ -6,16 +6,8 @@ $branch = $argv[2];
 $lastCommitMessage = exec('git log -1 HEAD --pretty=format:%s');
 
 if (stristr($lastCommitMessage, 'skip deploy') === false) {
-    echo "Downloading Rocketeer\n";
-
-    system('wget http://rocketeer.autopergamene.eu/versions/rocketeer.phar -O rocketeer.phar');
-    chmod('rocketeer.phar', 0755);
-
     echo "Deploying\n";
-    system("./rocketeer.phar deploy --on=$target --branch=$branch");
-
-    echo "Removing Rocketeer executable\n";
-    unlink('./rocketeer.phar');
+    system("rocketeer deploy --on=$target --branch=$branch");
 } else {
     echo "Deployment cancelled\n";
 }
