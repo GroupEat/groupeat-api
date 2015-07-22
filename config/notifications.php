af953031eb0f8f0a1d3516baa78d753752889cb2
Thibault Derousseaux <tib.dex@gmail.com>
2015-07-22 22:21:52 +0200

    Renaming APNS certificate file to hide it

--- config/notifications.php
+++ config/notifications.php
@@ -4,7 +4,7 @@ return [
 
     'gcmApiKey' => env('GCM_API_KEY', 'MISSING_GCM_API_KEY'),
 
-    'apnsCertificatePath' => base_path('APNS.pem'),
+    'apnsCertificatePath' => base_path('.apns.pem'),
 
     'apnsCertificatePassphrase' => env('APNS_CERTIFICATE_PASSPHRASE', 'MISSING_APNS_CERTIFICATE_PASSPHRASE'),
 
