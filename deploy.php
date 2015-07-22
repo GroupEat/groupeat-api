fbf9003cd55bdb1ad5ff04d0dbebe0a80306f034
Thibault Derousseaux <tib.dex@gmail.com>
2015-07-22 23:28:25 +0200

    Circle CI needs rocketeer to be installed locally

--- deploy.php
+++ deploy.php
@@ -6,8 +6,13 @@ $branch = $argv[2];
 $lastCommitMessage = exec('git log -1 HEAD --pretty=format:%s');
 
 if (stristr($lastCommitMessage, 'skip deploy') === false) {
+    echo "Downloading Rocketeer\n";
+
+    system('wget http://rocketeer.autopergamene.eu/versions/rocketeer.phar -O rocketeer.phar');
+    chmod('rocketeer.phar', 0755);
+
     echo "Deploying\n";
-    system("rocketeer deploy --on=$target --branch=$branch");
+    system("./rocketeer.phar deploy --on=$target --branch=$branch");
 } else {
     echo "Deployment cancelled\n";
 }
