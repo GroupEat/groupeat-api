237b521865abee6f48836ecd99849d84e9c71db4
Thibault Derousseaux <tib.dex@gmail.com>
2015-08-10 21:34:26 +0200

    Moving GenerateAdminerFiles logic to its dedicated command

--- app/Admin/Console/Adminer.php
+++ app/Admin/Console/Adminer.php
@@ -1,25 +1,61 @@
 <?php
 namespace Groupeat\Admin\Console;
 
-use Groupeat\Admin\Services\GenerateAdminerFiles;
 use Groupeat\Support\Console\Abstracts\Command;
+use GuzzleHttp\Client;
+use Illuminate\Contracts\Filesystem\Filesystem;
 
 class Adminer extends Command
 {
+    const THEME = 'pappu687';
+
     protected $signature = 'adminer';
     protected $description = "Generate the Adminer files to manage the DB";
 
-    private $generateAdminerFiles;
+    private $client;
+    private $filesystem;
 
-    public function __construct(GenerateAdminerFiles $generateAdminerFiles)
+    public function __construct(Filesystem $filesystem)
     {
         parent::__construct();
 
-        $this->generateAdminerFiles = $generateAdminerFiles;
+        $this->client = new Client;
+        $this->filesystem = $filesystem;
     }
 
     public function handle()
     {
-        $this->generateAdminerFiles->call($this->getOutput());
+        $this->line("Listing available versions");
+
+        $response = $this->client->get('https://api.github.com/repos/vrana/adminer/tags');
+
+        $latestVersion = collect(json_decode($response->getBody(), true))
+            ->map(function ($tag) {
+                return trim($tag['name'], 'v');
+            })
+            ->sortByDesc(null)
+            ->first();
+
+        $this->line("Latest version found: $latestVersion");
+        $this->line("Downloading PHP file");
+
+        $this->client->get("http://downloads.sourceforge.net/adminer/adminer-$latestVersion-en.php", [
+            'save_to' => storage_path('app/adminer.php'),
+        ]);
+
+        $this->line("Downloading CSS file");
+
+        $css = (string) $this->client
+            ->get('https://raw.github.com/vrana/adminer/master/designs/' . static::THEME . '/adminer.css')
+            ->getBody();
+
+        $this->line("Applying GroupEat theme");
+
+        $css = str_replace('#34495e', '#e74c3c', $css);
+        $css = str_replace(['#48A5BF', '#65ADC3', 'rgb(85, 112, 139)', '#2980b9'], '#e67e22', $css);
+
+        $this->line("Saving CSS file");
+
+        $this->filesystem->put('adminer.css', $css);
     }
 }
