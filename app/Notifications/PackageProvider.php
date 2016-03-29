9929de8f713389703427e3bfba5c7ef21b126dbd
Thibault Derousseaux <tib.dex@gmail.com>
2016-03-29 19:00:47 +0000

    Send 42 risky noisy notifications for each group order

--- app/Notifications/PackageProvider.php
+++ app/Notifications/PackageProvider.php
@@ -8,6 +8,7 @@ use Groupeat\Notifications\Values\ApnsCertificatePassphrase;
 use Groupeat\Notifications\Values\ApnsCertificatePath;
 use Groupeat\Notifications\Values\DeviceLocationFreshnessInMinutes;
 use Groupeat\Notifications\Values\GcmKey;
+use Groupeat\Notifications\Values\MaximumNumberOfRiskyNotifications;
 use Groupeat\Notifications\Values\NotificationsEnabled;
 use Groupeat\Orders\Events\GroupOrderHasBeenCreated;
 use Groupeat\Support\Providers\Abstracts\WorkbenchPackageProvider;
@@ -17,9 +18,10 @@ class PackageProvider extends WorkbenchPackageProvider
     protected $configValues = [
         ApnsCertificatePassphrase::class => 'notifications.apns.certificate_passphrase',
         ApnsCertificatePath::class => 'notifications.apns.certificate_path',
-        GcmKey::class => 'notifications.gcm.key',
-        NotificationsEnabled::class => 'notifications.enabled',
         DeviceLocationFreshnessInMinutes::class => 'notifications.device_location_freshness_in_minutes',
+        GcmKey::class => 'notifications.gcm.key',
+        MaximumNumberOfRiskyNotifications::class => 'notifications.maximum_number_of_risky_notifications',
+        NotificationsEnabled::class => 'notifications.enabled',
     ];
 
     protected $listeners = [
